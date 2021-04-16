9376a452edcae4edddc0aa12016997abf52648d9
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-04-16 16:05:57 +0530

    figure completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Figure from './Components/Figure';
 import Header from './Components/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
     </>
   );
 }
