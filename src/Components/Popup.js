4e6d6026c4973aba2034d2a34cd6a33abffc201f
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-04-16 16:48:26 +0530

    popus component

---
+++ src/Components/Popup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Popup = () => {
+    return (
+        <div class="popus-container" id="popus-container">
+            <div class="popup">
+                <h2 id="final-message"></h2>
+                <h3 id="final-message-reveal-word"></h3>
+                <button id="play-button">Play Again</button>
+            </div>
+        </div>
+    )
+}
+
+export default Popup
