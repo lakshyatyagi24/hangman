e787be538849886851576ee0b420e7b40f4c7b14
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-04-16 17:51:36 +0530

    completed word component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 import Figure from './Components/Figure';
 import Header from './Components/Header';
 import Word from './Components/Word';
 import WrongLetters from './Components/WrongLetters';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
     <>
       <Header />
       <div className="game-container">
         <Figure />
         <WrongLetters />
-        <Word />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
