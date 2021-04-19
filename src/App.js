26cb4ed3babf50fe8cc553035475bc37fcd2d53d
lakshyatyagi24 <tyagicoolboy22@gmail.com>
2021-04-20 01:22:05 +0530

    added notification

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Figure from './Components/Figure';
 import Header from './Components/Header';
+import Notification from './Components/Notification';
+import Popup from './Components/Popup';
 import Word from './Components/Word';
 import WrongLetters from './Components/WrongLetters';
+import { showNotification as show } from "./helpers/helpers";
 
 const words = ['application', 'programming', 'interface', 'wizard'];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
@@ -13,15 +16,50 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;      
+        if(playable && keyCode >= 65 && keyCode <= 90) {
+          const letter = key.toLowerCase();
+  
+          if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)) {
+              setCorrectLetters(currentLetters => [...currentLetters, letter]);
+            }
+            else {
+              show(setShowNotification);
+            }
+          }
+          else {
+            if(!wrongLetters.includes(letter)) {
+              setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+            }
+            else {
+              show(setShowNotification);              // showNotification();
+            }
+          }
+        }
+      
+  
+        window.addEventListener('keydown', handleKeydown);
+
+        return () => window.removeEventListener('keydown', handleKeydown);
+    }
+  }, [correctLetters, wrongLetters, playable]);
+
 
   return (
     <>
       <Header />
       <div className="game-container">
-        <Figure />
-        <WrongLetters />
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
+      <Popup />
+      <Notification showNotification={showNotification} />
     </>
   );
 }
